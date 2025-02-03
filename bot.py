1622e17bf464772cc70aa2064f156793f3bfe11b
lDanikl <Danyka.07@mail.ru>
2025-02-03 14:47:08 +0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ dp.middleware.setup(LoggingMiddleware())
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await bot.send_message(message.chat.id, "Привет! Я простой бот. Задайте мне вопрос или отправьте файл.")
+    await bot.send_message(message.chat.id, "Привет! Я геймерский_бот. Я могу посоветовать тебе игры из категорий каторые ты выберешь и у меня вы можете найти все игровые навинки и новости о них. Вот команды которые у меня есть /start, /категории, /тренеровка аима.")
 
 @dp.message_handler(content_types=types.ContentType.DOCUMENT)
 async def handle_document(message: types.Message):
@@ -23,16 +23,16 @@ async def handle_document(message: types.Message):
     await bot.download_file(file_info.file_path, f"./downloads/{message.document.file_name}")
     await bot.send_message(message.chat.id, f"Файл {message.document.file_name} получен и сохранен.")
 
-@dp.message_handler(lambda message: message.text.lower() == "список игр")
+@dp.message_handler(lambda message: message.text.lower() == "привет")
 async def greet_user(message: types.Message):
-    await message.reply("Выберите категорию игры введя команду /категории")
+    await message.reply("Привет! Как дела?")
 
 @dp.message_handler()
 async def echo(message: types.Message):
     print(message)
     user_message = message.text.lower()
     if 'привет' in user_message:
-        await bot.send_message(message.chat.id, 'https://rutube.ru/video/489bc98a94b7a5d90958493926419e42/')
+        await bot.send_message(message.chat.id, 'https://rutube.ru/video/2c0e40c8bbeb82da6d35600f9bfd162f/')
     elif 'как дела' in user_message:
         await bot.send_message(message.chat.id, 'У меня все хорошо, спасибо!')
     else:
@@ -40,6 +40,8 @@ async def echo(message: types.Message):
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
+
 
 
 
